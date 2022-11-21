862fe11895044ad9593330a757ad116b8ebaa1d6
Josh-ee <74482044+Josh-ee@users.noreply.github.com>
2022-11-21 01:21:51 -0600

    implemented axis limits
    implemented axis limits and fixed multiples of the same plots.
    A better solution may be to clear and plot all every time

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ Created on Sun Nov 20 13:40:36 2022
 
 @author: danie
 
-simple graph 
+simple graphing calculator
 
+To do:
+    Pop up the fatal error hook
+    movable legend
+    add icon
+    add clear history button
+    mouse click on graph
 """
 
 
@@ -63,61 +69,112 @@ class UI(QMainWindow):
         #point to UI file to use
         uic.loadUi("ui_files/graph_layout.ui", self)
         self.setContentsMargins(0,0,0,0) 
-        
-        #Below are the button connections
+        self.setWindowTitle("Josh's Graphing Calculator")
+        #Button connections
         self.browseButton.clicked.connect(self.browseFiles)
         self.plotButton.clicked.connect(self.plotFunction)
         self.clearButton.clicked.connect(self.clearPlot)
         
+        #configure defualts and rules
+        self.x_startSpinBox.setMinimum(-9999999999999)
+        self.x_startSpinBox.setMaximum(9999999999999)
+        self.x_endSpinBox.setMinimum(-9999999999999)
+        self.x_endSpinBox.setMaximum(9999999999999)
+        self.x_endSpinBox.setValue(10)
+        
+        self.y_startSpinBox.setMinimum(-9999999999999)
+        self.y_startSpinBox.setMaximum(9999999999999)
+        self.y_endSpinBox.setMinimum(-9999999999999)
+        self.y_endSpinBox.setMaximum(9999999999999)
+        self.y_endSpinBox.setValue(10)
+        
         self.historyTextEdit.setReadOnly(True)
-        #Below sets up the graph
+        
+        
+        #Set up the graph
         self.canvas = MatplotlibCanvas(self)
         self.toolbar = Navi(self.canvas, self.centralwidget)
         self.toolbar.setStyleSheet("background-color: rgb(255,255,255);")
         self.verticalLayout.addWidget(self.toolbar)
         self.verticalLayout.addWidget(self.canvas)
-
+        
+        self.on_plot = []
         
         self.show()
 
     def browseFiles(self):
-        print("yes")
         fname = QtWidgets.QFileDialog.getExistingDirectory(self, "Select Default Save Location", "C:/")
         if fname:
             self.save_location = fname + '/'
             plt.rcParams["savefig.directory"] = self.save_location
+            self.saveLabel.setText(f"Save Location: {self.save_location}")
         
     def press(self, event):
         if event.button == 3:
             pass
            # print(event.x, event.y)
-        
+    
         
     def plotFunction(self):
-        x = np.linspace(-5,5,100)
-        y = x**3
-        fucntion = self.functionEdit.text()
-        print(fucntion)
-        self.historyTextEdit.append(fucntion)
-        y = eval(fucntion)
+        #connect and format graph
         ax = self.canvas.axes
-       # ax.spines['left'].set_position('center')
-       # ax.spines['bottom'].set_position('center')
-       # ax.spines['right'].set_color('none')
-       # ax.spines['top'].set_color('none')
         ax.set_xlabel("x")
         ax.set_ylabel("y")
         ax.grid(True)
         ax.minorticks_on()
-        ax.axvline(x = 0, color = 'k')
+        ax.axvline(x = 0, color = 'k') #puts black lines on Axies
         ax.axhline(y = 0, color = 'k')
-        ax.plot(x,y,'g')
+        
+        #read values from spin boxes
+        x_start = self.x_startSpinBox.value()
+        x_end = self.x_endSpinBox.value()
+        y_start = self.y_startSpinBox.value()
+        y_end = self.y_endSpinBox.value()
+        x = np.linspace(x_start,x_end,100)
+        
+        #reads string in y = x line
+        fucntion = self.functionEdit.text()
+        
+        #print(self.on_plot)
+        
+        if 'x' in fucntion:
+            y = eval(fucntion) #turns string into funtion 
+            
+        else:
+            y = [eval(fucntion)]*100
+        
+        if fucntion in self.on_plot: #prevents plotting same line
+            ax.cla()
+            for item in self.on_plot:
+                if 'x' in item:
+                    y = eval(item) #turns string into funtion 
+                else:
+                    y = [eval(item)]*100
+                ax.plot(x,y)
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+            ax.grid(True)
+            ax.minorticks_on()
+            ax.axvline(x = 0, color = 'k') #puts black lines on Axies
+            ax.axhline(y = 0, color = 'k')
+            ax.axis(xmin=x_start,xmax=x_end)
+            ax.axis(ymin=y_start,ymax=y_end)
+            self.canvas.draw()
+            return
+        else:
+            self.on_plot.append(fucntion)
+            self.historyTextEdit.append(fucntion) 
+        
+        ax.plot(x,y)#'g')
+        ax.axis(xmin=x_start,xmax=x_end)
+        ax.axis(ymin=y_start,ymax=y_end)
+        
+       # ax.axis.ylim(self.y_startSpinBox.value(), self.y_endSpinBox.value())
         self.canvas.draw()
         
       
     def clearPlot(self):
-        print("clear")
-        
+        self.on_plot = []
         sip.delete(self.toolbar)
         sip.delete(self.canvas)
         ax = self.canvas.axes
@@ -127,7 +184,6 @@ class UI(QMainWindow):
         self.verticalLayout.addWidget(self.toolbar)
         self.verticalLayout.addWidget(self.canvas)
         self.historyTextEdit.append("cleared")
-       # ax.cla()  
         
 
       
